7924ffd9cef891d56d42e12cb61945c6cd44e53f
Dunbarry <RyanADunbar@gmail.com>
2016-07-20 19:35:57 -0600

    blarp

--- Player.js
+++ Player.js
@@ -28,7 +28,7 @@ var shoulder= {
               plating: 0,
               insulation: 0,
               weapons:["empty"],
-              equip:["ammo","ammo"]
+              equip:["30","30","empty"]
             },
             cylinder:["empty"]
 }
@@ -36,4 +36,8 @@ var shoulder= {
 
 $("#ignition").on('click', function(){
   $("#health span").html(shoulder.Player.health);
+  $("#armor span").html(shoulder.aRm["1"].plating);
+  $("#insulation span").html(shoulder.aRm["1"].insulation);
+  $("#weapon1").html(shoulder.aRm["1"].weapons[0]);
+  $("#ammo1 span").html(shoulder.aRm["1"].equip[0]);
 })
