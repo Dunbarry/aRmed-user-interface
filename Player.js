54082216cae5b36dbd1f849fb4806ff0c2593b43
Dunbarry <RyanADunbar@gmail.com>
2016-07-22 10:17:14 -0600

    Stats fully updating.

--- Player.js
+++ Player.js
@@ -27,7 +27,7 @@ var shoulder= {
             4:{
               plating: 0,
               insulation: 0,
-              weapons:["empty"],
+              weapons:["empty","empty"],
               equip:["30","30","empty"]
             },
             cylinder:["empty"]
@@ -37,22 +37,20 @@ var shoulder= {
 $("#ignition").on('click', function(){
   $("#health span").html(shoulder.Player.health);
 /*Q1*/
-  $("#armor1 span").html(shoulder.aRm["1"].plating);
+  $("#armor1").html("Armor: "+shoulder.aRm["1"].plating+"  Insulation: "+shoulder.aRm["1"].insulation);
   $("#insulation1 span").html(shoulder.aRm["1"].insulation);
   $("#weapon11").html(shoulder.aRm["1"].weapons[0]);
   $("#weapon12").html(shoulder.aRm["1"].weapons[1]);
 /*Q2*/
-  $("#armor2 span").html(shoulder.aRm["2"].plating);
-  $("#insulation2 span").html(shoulder.aRm["2"].insulation);
+  $("#armor2").html("Armor: "+shoulder.aRm["2"].plating+"  Insulation: "+shoulder.aRm["2"].insulation);
   $("#weapon21").html(shoulder.aRm["2"].weapons[0]);
   $("#weapon22").html(shoulder.aRm["2"].weapons[1]);
 /*Q3*/
-  $("#armor3 span").html(shoulder.aRm["3"].plating);
-  $("#insulation3 span").html(shoulder.aRm["3"].insulation);
+  $("#armor3").html("Armor: "+shoulder.aRm["3"].plating+"  Insulation: "+shoulder.aRm["3"].insulation);
   $("#weapon31").html(shoulder.aRm["3"].weapons[0]);
   $("#weapon32").html(shoulder.aRm["3"].weapons[1]);
-/*Q3*/
-  $("#armor4 span").html(shoulder.aRm["4"].plating);
+/*Q4*/
+  $("#armor4").html("Armor: "+shoulder.aRm["4"].plating+"  Insulation: "+shoulder.aRm["4"].insulation);
   $("#insulation4 span").html(shoulder.aRm["4"].insulation);
   $("#weapon41").html(shoulder.aRm["4"].weapons[0]);
   $("#weapon42").html(shoulder.aRm["4"].weapons[1]);
